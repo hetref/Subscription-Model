6e36368879f658850f9506b4aae33c6f8b9beba7
Aryan shinde <shindearyan179@gmail.com>
2024-02-11 05:15:11 +0530

    Fix

--- app/page.js
+++ app/page.js
@@ -32,8 +32,6 @@ export default function Home() {
       if (!isLoading) {
         if (firebaseUser) {
           router.push("/account");
-        } else {
-          router.push("/");
         }
       }
     };
