a8054a3e8b1a86d29b2ebaab2463902644cd96aa
Aryan shinde <shindearyan179@gmail.com>
2024-02-11 05:30:29 +0530

    Fixed

--- app/account/page.jsx
+++ app/account/page.jsx
@@ -51,41 +51,43 @@ const Account = () => {
 
   return (
     <div className="flex justify-center items-center h-[100dvh]">
-      <div className="flex flex-col items-center">
-        <span className="mb-2">
-          Signed in as {userName} ({email})
-        </span>
-        {isPremium ? (
-          <p className="mb-6 text-emerald-900 text-2xl">
-            You are a <b>premium user</b>
-          </p>
-        ) : (
-          <p className="mb-6 text-red-900">
-            You are a <b>free user</b>
-          </p>
-        )}
-        {isPremium ? (
+      {!loading && (
+        <div className="flex flex-col items-center">
+          <span className="mb-2">
+            Signed in as {userName} ({email})
+          </span>
+          {isPremium ? (
+            <p className="mb-6 text-emerald-900 text-2xl">
+              You are a <b>premium user</b>
+            </p>
+          ) : (
+            <p className="mb-6 text-red-900">
+              You are a <b>free user</b>
+            </p>
+          )}
+          {isPremium ? (
+            <button
+              onClick={manageSubscription}
+              className="bg-black text-white px-[16px] py-[8px] rounded"
+            >
+              Manage Subscription
+            </button>
+          ) : (
+            <button
+              onClick={upgradeToPremium}
+              className="bg-black text-white px-[16px] py-[8px] rounded"
+            >
+              Upgrade to Premium
+            </button>
+          )}
           <button
-            onClick={manageSubscription}
-            className="bg-black text-white px-[16px] py-[8px] rounded"
+            onClick={signOut}
+            className="bg-red-500/90 mt-6 text-white px-[16px] py-[8px] rounded"
           >
-            Manage Subscription
+            Sign Out
           </button>
-        ) : (
-          <button
-            onClick={upgradeToPremium}
-            className="bg-black text-white px-[16px] py-[8px] rounded"
-          >
-            Upgrade to Premium
-          </button>
-        )}
-        <button
-          onClick={signOut}
-          className="bg-red-500/90 mt-6 text-white px-[16px] py-[8px] rounded"
-        >
-          Sign Out
-        </button>
-      </div>
+        </div>
+      )}
     </div>
   );
 };
